e1542d84ef78a32b77a46a2db33010ab12dc9f45
Lonky131 <kl50222@fer.hr>
2022-01-02 16:53:34 +0100

    1hour33mins

--- src/components/TwootItem.vue
+++ src/components/TwootItem.vue
@@ -1,35 +1,30 @@
 <template>
-  <div class="twoot-item" @click="favouriteTwoot(twoot.id)">
-      <div class="user-profile__twoot">
-          <div class="twoot-item__user">
-              @{{ username }}
-          </div>
-          <div class="twoot-item__content">
-              {{ twoot.content }}
-          </div>
+  <div class="twoot-item">
+    <div class="user-profile__twoot">
+      <div class="twoot-item__user">
+        @{{ username }}
       </div>
+      <div class="twoot-item__content">
+        {{ twoot.content }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "TwootItem",
-    props: {
-        username: {
-            type: String,
-            required: true
-        },
-        twoot:{
-            type: Object,
-            required: true
-        }
+  name: "TwootItem",
+  props: {
+    username: {
+      type: String,
+      required: true
     },
-    methods:{
-        favouriteTwoot(id){
-            this.$emit('favourite', id);
-        }
+    twoot: {
+      type: Object,
+      required: true
     }
-}
+  }
+};
 </script>
 
 <style>
